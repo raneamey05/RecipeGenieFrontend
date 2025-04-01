60ff880db86113c8a78d56d3fe5f2453e97d1b74
raneamey05 <raneamey37@gmail.com>
2025-04-01 14:57:49 +0530

    Update AboutUs.js

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,10 +1,11 @@
 import React from "react";
+import "./aboutus.css";
 
 const AboutUs = () => {
   return (
     <div>
       <h2>About Us</h2>
-      <p>Learn more about our recipe recommendation system!</p>
+      <p>Learn more about our RecipeGenie!</p>
     </div>
   );
 };
